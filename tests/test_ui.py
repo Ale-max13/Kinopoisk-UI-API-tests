bef1db48bd262e0b2557bbac92189212647bb17d
Ale-max13 <kitiale1313@gmail.com>
2025-09-14 18:40:15 +0200

    add: добавлены Allure-steps в UI тесты, исправлены ошибки flake8

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,104 +10,139 @@ import config
 
 
 @pytest.mark.ui
+@allure.feature("UI")
+@allure.story("Поиск")
 def test_search_bar_is_displayed():
-    service = Service(executable_path="./yandexdriver.exe")
-    options = webdriver.ChromeOptions()
-    options.binary_location = config.YANDEX_BROWSER_PATH
-    options.add_argument("--disable-logging")
-    options.add_argument("--log-level=3")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--disable-blink-features=AutomationControlled")
+    with allure.step("Запуск браузера"):
+        service = Service(executable_path="./yandexdriver.exe")
+        options = webdriver.ChromeOptions()
+        options.binary_location = config.YANDEX_BROWSER_PATH
+        options.add_argument("--disable-logging")
+        options.add_argument("--log-level=3")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        driver = webdriver.Chrome(service=service, options=options)
 
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get("https://www.kinopoisk.ru/")
+    with allure.step("Открываем сайт Кинопоиск"):
+        driver.get("https://www.kinopoisk.ru/")
 
-    search_bar = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.NAME, "kp_query"))
-    )
-    assert search_bar.is_displayed()
-    driver.quit()
-
-
-@pytest.mark.ui
-def test_movie_card_is_displayed():
-    service = Service(executable_path="./yandexdriver.exe")
-    options = webdriver.ChromeOptions()
-    options.binary_location = config.YANDEX_BROWSER_PATH
-    options.add_argument("--disable-logging")
-    options.add_argument("--log-level=3")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get("https://www.kinopoisk.ru/")
-
-    search_bar = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.NAME, "kp_query"))
-    )
-    search_bar.send_keys("Оно\n")
-
-    movie_card = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href*="/film/"]'))
-    )
-    assert movie_card.is_displayed()
-    driver.quit()
-
-
-@pytest.mark.ui
-def test_popular_movies_page():
-    service = Service(executable_path="./yandexdriver.exe")
-    options = webdriver.ChromeOptions()
-    options.binary_location = config.YANDEX_BROWSER_PATH
-    options.add_argument("--disable-logging")
-    options.add_argument("--log-level=3")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get("https://www.kinopoisk.ru/lists/movies/popular/")
-
-    movie_card = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href*="/film/"]'))
-    )
-    assert movie_card.is_displayed()
-    driver.quit()
-
-
-@pytest.mark.ui
-def test_main_page_title():
-    service = Service(executable_path="./yandexdriver.exe")
-    options = webdriver.ChromeOptions()
-    options.binary_location = config.YANDEX_BROWSER_PATH
-    options.add_argument("--disable-logging")
-    options.add_argument("--log-level=3")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get("https://www.kinopoisk.ru/")
-
-    assert "Кинопоиск" in driver.title
-    driver.quit()
-
-
-@pytest.mark.ui
-def test_login_button_is_displayed():
-    service = Service(executable_path="./yandexdriver.exe")
-    options = webdriver.ChromeOptions()
-    options.binary_location = config.YANDEX_BROWSER_PATH
-    options.add_argument("--disable-logging")
-    options.add_argument("--log-level=3")
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-
-    driver = webdriver.Chrome(service=service, options=options)
-    driver.get("https://www.kinopoisk.ru/")
-
-    login_button = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//button[contains(text(), 'Войти')]")
+    with allure.step("Проверяем наличие строки поиска"):
+        search_bar = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.NAME, "kp_query"))
         )
-    )
-    assert login_button.is_displayed()
+        assert search_bar.is_displayed()
+
+    driver.quit()
+
+
+@pytest.mark.ui
+@allure.feature("UI")
+@allure.story("Карточка фильма")
+def test_movie_card_is_displayed():
+    with allure.step("Запуск браузера"):
+        service = Service(executable_path="./yandexdriver.exe")
+        options = webdriver.ChromeOptions()
+        options.binary_location = config.YANDEX_BROWSER_PATH
+        options.add_argument("--disable-logging")
+        options.add_argument("--log-level=3")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        driver = webdriver.Chrome(service=service, options=options)
+
+    with allure.step("Открываем сайт Кинопоиск"):
+        driver.get("https://www.kinopoisk.ru/")
+
+    with allure.step("Вводим текст в поиск"):
+        search_bar = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.NAME, "kp_query"))
+        )
+        search_bar.send_keys("Оно\n")
+
+    with allure.step("Проверяем наличие карточки фильма"):
+        movie_card = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, 'a[href*="/film/"]')
+            )
+        )
+        assert movie_card.is_displayed()
+
+    driver.quit()
+
+
+@pytest.mark.ui
+@allure.feature("UI")
+@allure.story("Популярные фильмы")
+def test_popular_movies_page():
+    with allure.step("Запуск браузера"):
+        service = Service(executable_path="./yandexdriver.exe")
+        options = webdriver.ChromeOptions()
+        options.binary_location = config.YANDEX_BROWSER_PATH
+        options.add_argument("--disable-logging")
+        options.add_argument("--log-level=3")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        driver = webdriver.Chrome(service=service, options=options)
+
+    with allure.step("Открываем страницу популярных фильмов"):
+        driver.get("https://www.kinopoisk.ru/lists/movies/popular/")
+
+    with allure.step("Проверяем наличие карточки фильма"):
+        movie_card = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, 'a[href*="/film/"]')
+            )
+        )
+        assert movie_card.is_displayed()
+
+    driver.quit()
+
+
+@pytest.mark.ui
+@allure.feature("UI")
+@allure.story("Главная страница")
+def test_main_page_title():
+    with allure.step("Запуск браузера"):
+        service = Service(executable_path="./yandexdriver.exe")
+        options = webdriver.ChromeOptions()
+        options.binary_location = config.YANDEX_BROWSER_PATH
+        options.add_argument("--disable-logging")
+        options.add_argument("--log-level=3")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        driver = webdriver.Chrome(service=service, options=options)
+
+    with allure.step("Открываем сайт Кинопоиск"):
+        driver.get("https://www.kinopoisk.ru/")
+
+    with allure.step("Проверяем заголовок страницы"):
+        assert "Кинопоиск" in driver.title
+
+    driver.quit()
+
+
+@pytest.mark.ui
+@allure.feature("UI")
+@allure.story("Кнопка входа")
+def test_login_button_is_displayed():
+    with allure.step("Запуск браузера"):
+        service = Service(executable_path="./yandexdriver.exe")
+        options = webdriver.ChromeOptions()
+        options.binary_location = config.YANDEX_BROWSER_PATH
+        options.add_argument("--disable-logging")
+        options.add_argument("--log-level=3")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        driver = webdriver.Chrome(service=service, options=options)
+
+    with allure.step("Открываем сайт Кинопоиск"):
+        driver.get("https://www.kinopoisk.ru/")
+
+    with allure.step("Проверяем наличие кнопки 'Войти'"):
+        login_button = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//button[contains(text(),'Войти')]")
+            )
+        )
+        assert login_button.is_displayed()
+
     driver.quit()
