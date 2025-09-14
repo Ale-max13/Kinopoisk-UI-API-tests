e8937d8de7a8fb1d5ae0751228c6c4099311c3a8
Ale-max13 <kitiale1313@gmail.com>
2025-09-14 18:02:09 +0200

    fix: убран .env из репозитория, добавлен .env.example и обновлён config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-YANDEX_BROWSER_PATH = os.getenv("YANDEX_BROWSER_PATH")
--- tests/config.py
+++ tests/config.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 YANDEX_BROWSER_PATH = os.getenv("YANDEX_BROWSER_PATH")
+BASE_URL = os.getenv("BASE_URL")
+API_TOKEN = os.getenv("API_TOKEN")
